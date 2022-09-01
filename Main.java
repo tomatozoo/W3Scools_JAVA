379f9152b0477e35f2cddd7aab6c3a0a87e20547
tomatozoo <enkeejuniour@naver.com>
2022-09-01 11:21:20 +0900

    java tutorial

---
+++ Main.java
@@ -0,0 +1,11 @@
+// class 명은 항상 대문자로 시작
+// 클래스 이름이 Main
+// 파일 이름은 클래스 이름과 일치해야 함
+public class Main {
+    // 메소드
+    public static void main(String[] args){
+        // 기본 출력
+        // System은 자바의 내장 클래스 Print line의 줄임말
+        System.out.println("Hello world");
+    }
+}
